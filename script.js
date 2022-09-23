2fc44576ea682068941ff0c969ec65828af2b4ad
Anitha Nagadasari <anitha.nagadasarik95@gmail.com>
2022-09-23 19:34:28 +0530

    final commit

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const quoteAuthor = document.querySelector(".author");
 function randomeQuoteGenerator(){
   // Fetching the data from API
 
-  fetch("http://quotes.stormconsultancy.co.uk/random.json")
+  //fetch("http://quotes.stormconsultancy.co.uk/random.json")
+  fetch("https://api.quotable.io/random")
   .then(response => response.json())
   .then(data => {
-    quoteText.innerText = `“${data.quote}”`;
+    quoteText.innerText = `${data.content}`;
     quoteAuthor.innerText = `— ${data.author}`;
   }); 
 }
